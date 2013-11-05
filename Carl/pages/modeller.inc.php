ca7b016dd6525223c9a6219982a46d7c83fda158
s198756 <hbkrules@live.no>
2013-11-05 11:04:50 +0100

    dokumnetstruktur

---
+++ Carl/pages/modeller.inc.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+  <body>
+    <article>
+      <header><h1>3D-Printere-header</h1></header>
+	  <section id="models">
+	    <h2>Utvalgte modeller:</h2>
+
+		<div class="outer" id="first">
+		  <div class="inner">
+		    <h3>BFB 3Dtouch</h3>
+			<img src="3dprinter_0.jpg" alt="Printer nr1" width="150" id="printer1">
+		  </div>
+		  <p>3Dtouch er ideel for hjem, skole, ...</p>
+		  <button type="button">Les mer</button>
+		</div>
+
+		<div class="outer" id="second">
+		  <div class="inner" id="inner2">
+		    <h3>Cube</h3>
+		    <img src="cube.jpg" alt="Printer nr2" width="150" id="printer2">
+		  </div>
+		  <p>Cube er printeren som får deg igang...</p>
+		  <button type="button">Les mer</button>
+		</div>
+
+		<div class="outer" id="third">
+		  <div class="inner" id="inner3">
+		    <h3>RepRap</h3>
+			<img src="reprap.jpg" alt="Printer nr3" width="150" height="130" id="printer3">
+		  </div>
+		  <p>Denne printeren kan replisere</p>
+		  <button type="button">Les mer</button>
+		</div>
+
+		<div class="outer" id="fourth">
+		  <div class="inner" id="inner4">
+		    <h3>ProJet 660</h3>
+			<img src="projet660.jpg" alt="Printer nr4" width="150" height="130" id="printer4">
+		  </div>
+		  <p>Dette er en "high end" maskin...</p>
+		  <button type="button">Les mer</button>
+		</div>
+
+		<div class="outer" id="fifth">
+		  <div class="inner" id="inner5">
+		    <h3>Printrbot Simple</h3>
+			<img src="printrbot.jpg" alt="Printer nr5" width="150" height="130" id="printer5">
+		  </div>
+		  <p>En billigmaskin fra Printrbot</p>
+		  <button type="button">Les mer</button>
+		</div>
+
+		<div class="outer" id="sixth">
+		  <div class="inner" id="inner6">
+		    <h3>Form1</h3>
+			<img src="form1.png" alt="Printer nr6" width="150" height="130" id="printer6">
+		  </div>
+		  <p>Ny spennende teknologi</p>
+		  <button type="button">Les mer</button>
+		</div>
+
+	  </section>
+	</article>
+  </body>
+</html>
