431178efd80bb49cc02c94d17f8357149a9129a5
eivsch <eivindschulstad@gmail.com>
2013-11-07 18:56:26 +0100

    Update 7.11 - ES
    Oppdatert siden med felles css, javascript på "les mer" knappene og mer
    tekst

---
+++ underside-ES/scriptES.js
@@ -0,0 +1,11 @@
+//toggle-knapp for skjulte section-elementer
+var tekst;
+function toggleTekst(id){
+  tekst=id;
+  $(tekst).toggle(115);
+}
+/*$(document).ready(function(){
+    $("#cubeShow").click(function(){
+        $("div#cubeText").toggle(100);
+    });
+});*/
