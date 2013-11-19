691b4b29900202988da7d0d0a10b094d5829c9ba
Thomso0809 <thomso0809@gmail.com>
2013-11-19 15:26:27 +0100

    Page, CSS, bilder og ikon
    Endret på CSS og page.
    Lagt til bilder og ikon.

--- pages/design.inc.php
+++ pages/design.inc.php
@@ -2,88 +2,119 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="styles/stylesheet2.css">
-        <title>3DM-filer</title>
+        <title>3D-filer</title>
     </head>
     <body id="alpha">
         <section class="top">
+            <img src="bilder/testikon2.png" class="ikoner" id="ikontest">
+            <h1 class="undersiderOverskrift" id="overskrift">3D-filer</h1>
         <div id="test">
             </div>
             <section id="design1">
                 <h1 id="h1">Nokia-deksel</h1>
                 <p id="tekst1">
-                    Deksel til Nokia XTC8
-                    Dimensjoner: 20x8cm
-                    Størrelse: 500k
+                    Deksel til Nokia XTC8. <br/>
+                    Dimensjoner: 20x8cm <br/>
+                    Størrelse: 500kb <br/>
                 </p>
                 <div id="bilde1">
-                    <img src="bilder/design.jpg" alt="Nokia deksel-design" width="110" height="170">
+                    <img src="bilder/design.jpg" alt="Nokia deksel-design. Hentet fra: http://conversations.nokia.com/2013/02/25/tweet-print-win-at-mwc-the-worlds-first-live-social-3d-printing-experience/" width="110" height="170">
                 </div>
-                <div id="lastned1">
-                <a href="3designs/human.3DM"><p id="tekstA">Last ned</p></a>
-                </div>
+                <!--<div id="lastned1">-->
+                <form method="get" action="3designs/human.3dm">
+                    <!-- Alle nedlastningslinkene er de samme. Les mer i rapporten under punkt X. -->
+                        <button class="buttonStyle" type="submit" id="lastned1">Last ned</button>
+                    </form>
+                
+                <!--</div>-->
             </section>
             
             <section id="design2">
-                <h1 id="h2">3DM-filer #1</h1>
-                <p id="tekst2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget pulvinar mi, et tristique sapien.
-                               Phasellus aliquet ligula vitae orci fermentum ornare. Proin ut urna id leo aliquam tempor.</p>
+                <h1 id="h2">Hånd med golfball</h1>
+                <p id="tekst2">
+                    Hånd som holder en golfball. <br/>
+                    Dimensjoner: 28x12cm <br/>
+                    Størrelse: 678kb <br/>
+                </p>
                 <div id="bilde2">
-                    <img src="bilder/design.jpg" alt="Nokia deksel-design" width="110" height="170">
+                    <img src="bilder/design2.png" alt="Hånd med golfball. Hentet fra:http://www.cgtrader.com/3d-models/character-people/anatomy/hand-with-golf-ball-rigged" width="110" height="170">
                 </div>
-                <div id="lastned2">
-                <a href="3designs/human.3DM"><p id="tekstB">Last ned</p></a>
-                </div>
+                <!--<div id="lastned2">-->
+               <form method="get" action="3designs/human.3dm">
+                        <button class="buttonStyle" type="submit" id="lastned2">Last ned</button>
+                    </form>
+                <!--</div>-->
             </section>
             
             <section id="design3">
-                <h1 id="h3">3DM-filer #1</h1>
-                <p id="tekst3">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget pulvinar mi, et tristique sapien.
-                               Phasellus aliquet ligula vitae orci fermentum ornare. Proin ut urna id leo aliquam tempor.</p>
+                <h1 id="h3">Datamus</h1>
+                <p id="tekst3">
+                    Mus til data. <br/>
+                    Dimensjoner: 12x6cm <br/>
+                    Størrelse: 248kb <br/>
+                </p>
                 <div id="bilde3">
-                    <img src="bilder/design.jpg" alt="Nokia deksel-design" width="110" height="170">
+                    <img src="bilder/design3.jpg" alt="Datamus. Hentet fra: http://www.cgtrader.com/3d-models/electronics/computer/computer-mouse--10" width="110" height="170">
                 </div>
-                <div id="lastned3">
-                <a href="3designs/human.3DM"><p id="tekstC">Last ned</p></a>
-                </div>
+                <!--<div id="lastned3">-->
+                <form method="get" action="3designs/human.3dm">
+                        <button class="buttonStyle" type="submit" id="lastned3">Last ned</button>
+                    </form>
+                <!--</div>-->
             </section>
             
             <section id="design4">
-                <h1 id="h4">3DM-filer #1</h1>
-                <p id="tekst4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget pulvinar mi, et tristique sapien.
-                               Phasellus aliquet ligula vitae orci fermentum ornare. Proin ut urna id leo aliquam tempor.</p>
+                <h1 id="h4">Veener Audio-speaker</h1>
+                <p id="tekst4">
+                    Veener høytalere. <br/>
+                    Dimensjoner: 78x23cm <br/>
+                    Størrelse: 645kb <br/>
+                </p>
                 <div id="bilde4">
-                    <img src="bilder/design.jpg" alt="Nokia deksel-design" width="110" height="170">
+                    <img src="bilder/design4.jpg" alt="Veener Audio-speaker. Hentet fra: http://www.cgtrader.com/3d-models/electronics/audio/wood-veneer-audio-speakers" width="110" height="170">
                 </div>
-                <div id="lastned4">
-                <a href="3designs/human.3DM"><p id="tekstD">Last ned</p></a>
-                </div>
+                <!--<div id="lastned4">-->
+                <form method="get" action="3designs/human.3dm">
+                        <button class="buttonStyle" type="submit" id="lastned4">Last ned</button>
+                    </form>
+                <!--</div-->
             </section>
             
             <section id="design5">
-                <h1 id="h5">3DM-filer #1</h1>
-                <p id="tekst5">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget pulvinar mi, et tristique sapien.
-                               Phasellus aliquet ligula vitae orci fermentum ornare. Proin ut urna id leo aliquam tempor.</p>
+                <h1 id="h5">058 Stol</h1>
+                <p id="tekst5">
+                    Stol-design 058. <br/>
+                    Dimensjoner: 50x24cm <br/>
+                    Størrelse: 900kb <br/>
+                </p>
                 <div id="bilde5">
-                    <img src="bilder/design.jpg" alt="Nokia deksel-design" width="110" height="170">
+                    <img src="bilder/design5.jpg" alt="Stol-design 058. Hentet fra: http://www.cgtrader.com/3d-models/furniture/chair/chair-design-058" width="110" height="170">
                 </div>
-                <div id="lastned5">
-                <a href="3designs/human.3DM"><p id="tekstE">Last ned</p></a>
-                </div>
+                <!--<div id="lastned5">-->
+                    <form method="get" action="3designs/human.3dm">
+                        <button class="buttonStyle" type="submit" id="lastned5">Last ned</button>
+                    </form>
+                <!--</div>-->
             </section>
             
             <section id="design6">
-                <h1 id="h6">3DM-filer #1</h1>
-                <p id="tekst6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas eget pulvinar mi, et tristique sapien.
-                               Phasellus aliquet ligula vitae orci fermentum ornare. Proin ut urna id leo aliquam tempor.</p>
+                <h1 id="h6">Fiolin</h1>
+                <p id="tekst6">
+                    Fiolin med bue. <br/>
+                    Dimensjoner: 38x15cm <br/>
+                    Størrelse: 765kb <br/>
+                </p>
                 <div id="bilde6">
-                    <img src="bilder/design.jpg" alt="Nokia deksel-design" width="110" height="170">
+                    <img src="bilder/design6.png" alt="Fiolin med bue. Hentet fra: http://www.cgtrader.com/3d-models/sport-hobby/musical-instrument/violin-and-bow" width="110" height="170">
                 </div>
-                <div id="lastned6">
-                <a href="3designs/human.3DM"><p id="tekstF">Last ned</p></a>
-                </div>
+                <!--<div id="lastned6">-->
+                    <form method="get" action="3designs/human.3dm">
+                        <button class="buttonStyle" type="submit" id="lastned6">Last ned</button>
+                    </form>
+                <!--</div-->
             </section>
             <div id="test2">
-                <p>test</p>
+                <p></p>
             </div>
      </section>
     </body>
