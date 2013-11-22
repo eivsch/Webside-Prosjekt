dfb7d6f084f43d7e1dd6d57056bd27156d1a27db
eivsch <s198752@stud.hioa.no>
2013-11-22 11:55:33 +0100

    update - tekst

--- pages/modeller.inc.php
+++ pages/modeller.inc.php
@@ -17,10 +17,10 @@
 	      <p> Det finnes svært mange modeller, og
 		     prisene kan variere fra noen tusenlapper, til millioner av kroner. Idag regnes
 			 3D-Printerne til under 30 000 kr som billige, disse har bare vært på markedet i 2-3 år.
-			 Det finnes flere ulike teknologier å velge mellom. Dersom du ønsker deg en printer
-			 til hjemmebruk, er nok en FDM-maskin stedet å begynne. Det står for Fused Deposition
-			 Modeling, og denne typen maskiner er de klart billigste. Videre finnes det nå rimelige SLA og DLP
-			 maskiner, en nyere teknologi som gir bedre printing. Les mer om ulike typer maskiner
+			 I valget av printer er det lurt å være klar over hva slags printerteknologi modellen den bruker.
+			 Dersom du f.eks. ønsker deg en printer til hjemmebruk, vil en FDM-maskin være et naturlig valg.
+			 Det står for Fused Deposition Modeling, og denne typen maskiner er de klart billigste. Det finnes såklart også
+			 nyere teknologier som gir både raskere og mer detaljert printing, men det tar tid før disse synker i pris. Les mer om ulike typer maskiner
 			 nedenfor:
              </p>
 		</div>
@@ -29,7 +29,7 @@
 	      <p> Med en 3D-Printer kan du skrive ut masse forskjellige
 		     plastdingser hjemme i din egen stue. Det er til og med blitt printet ut fullt fungerende
 			 våpen med enkle 3D-printere. Av andre ting kan også nevnes flytende metall, mikrobatteri og NASA
-			 har faktisk skrevet ut rakettdeler! Printerne for hjemmebruk idag er foreløpig ganske enkle og
+			 har faktisk skrevet ut rakettdeler. Printerne for hjemmebruk idag er foreløpig ganske enkle og
 			 begrenser seg til å skrive ut ting i plast, vanligvis <a href="http://www.bitsfrombytes.com/content/3d-printing-materials">PLA og ABS</a>.
 			 Likevel er det masser av ting du kan lage og potensielt er det penger å spare på å skaffe seg en 3D-Printer.
 			 Lær mer om de ulike modellene nedenfor:</p>
@@ -45,7 +45,7 @@
 		  </div>
 		  <div>En FDM printer i øvre sjikt. Cube fra<div class="skjult" id="cubeText" hidden> Cubify
 		  er en fin maskin for å komme igang med 3D-printing, da den er relativt billig og enkel i bruk.
-		  Den kan printe i både ABS og PLA-materialer, og du kan velge mellom forskjellige farger (bare en av gangen).
+		  Den kan printe i både ABS og PLA-materialer, og du kan velge mellom forskjellige farger (men bare en av gangen).
 		  <br>Pris: Rundt 12 000 kr.</div></div>
 		  <button class="buttonStyle" type="button" onclick="toggleTekst('#cubeText', '#knapp1');" aria-expanded="false" id="knapp1" tabindex="5">Les mer</button>
 		</div>
@@ -57,7 +57,7 @@
 		    <p class="byline">Hentet fra <a href="http://www.3dprint.no/">3Dprint.no</a> </p>
 		  </div>
 		  <div>3Dtouch er en dyrere "billigprinter", <div class="skjult" id="bfbText" hidden>Den er basert
-		  på samme teknologi som Cube, men er vesentlig større. Denne passer nok bedre for f.eks skole eller
+		  på samme teknologi som Cube, men er vesentlig større og mer avansert. Godt egnet for skole eller
 		  bedriftsbruk.<br>Pris: Rundt 30 000 kr.
 		  </div></div>
 		  <button class="buttonStyle" type="button" onclick="toggleTekst('#bfbText','#knapp2');" aria-expanded="false" id="knapp2" tabindex="6">Les mer</button>
@@ -70,7 +70,7 @@
 			<p class="byline">Hentet fra <a href="http://www.utra.ca/projects/reprap">utra.ca</a> </p>
 		  </div>
 		  <div>RepRap er i seg selv en ganske enkel<div class="skjult" id="repRapText" hidden> printer. Likevel
-		  er den en av de mest spennende da den faktisk er kapabel til å printe ut sine egne deler!
+		  er den en av de mest spennende da den faktisk er kapabel til å printe ut sine egne deler.
 		  Den bruker en versjon av FDM-teknologien.
 		  Det hele er basert på et prosjekt av dr. Adrian Bowyer, les mer
 		  om det <a href="http://reprap.org/wiki/RepRap">her</a>.</div></div>
@@ -87,7 +87,7 @@
 		  </div>
 		  <div>Dette er en "high end" maskin<div class="skjult" id="proJetText" hidden>fra 3Dsystems.
 		  Den printer svært detaljert, og er basert på teknologien CJP (ColourJet Printing), som blant annet gjør at den kan printe
-		  ut i forskjellige farger. Brukes av mange større bedrifter i Norge idag, blant annet arkitektfirmaet Snøhetta.
+		  en modell i forskjellige farger. Brukes av mange større bedrifter i Norge idag, blant annet arkitektfirmaet Snøhetta.
 		  <br>Pris: Fra 500 000 kr.</div></div>
 		  <button class="buttonStyle" type="button" onclick="toggleTekst('#proJetText', '#knapp4');" aria-expanded="false" id="knapp4" tabindex="8">Les mer</button>
 		</div>
@@ -113,7 +113,7 @@
 		  </div>
 		  <div>Form1 bruker stereolitografi (SLA),<div class="skjult" id="form1Text" hidden>som
 		  er en relatitvt ny printerteknologi på hjemmemarkedet. Denne gir svært detaljrike
-          utskrifter, og er ikke så altfor dyr.
+          utskrifter, og er heller ikke så altfor dyr.
 		  <br>Pris: Fra 20 000 kr + frakt + avgifter.</div></div>
 		  <button class="buttonStyle" type="button" onclick="toggleTekst('#form1Text', '#knapp6');" aria-expanded="false" id="knapp6" tabindex="10">Les mer</button>
 		</div>
@@ -121,4 +121,3 @@
 
 		</section>
       </section>
-     
